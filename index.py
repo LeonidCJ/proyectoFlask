c795227a252390bc0ba801e972549c2ef96936da
LeonidCJ <143663889+LeonidCJ@users.noreply.github.com>
2026-04-25 10:50:28 -0500

    corregir

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -17,4 +18,5 @@ def login():
         return render_template("login.html", mensaje="Usuario o contraseña incorrectos")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
